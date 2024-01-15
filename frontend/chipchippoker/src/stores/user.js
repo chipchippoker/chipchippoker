ffc815f3d1efc78f36d1278c5bd0e5d70a89af19
yebin <yebin4684@gmail.com>
2024-01-15 10:09:33 +0900

    fix: store 병합

--- frontend/chipchippoker/src/stores/user.js
+++ frontend/chipchippoker/src/stores/user.js
@@ -14,30 +14,27 @@ export const useUserStore = defineStore('counter', () => {
   const checkNickName = function (nickName){
   const userIcon = ref(null)
   
-  // 카카오 회원가입 함수
+  
+  // 카카오 회원가입
   const  kakaoSignUp = function(nickName){
     const payload = {
       'nickname':nickName
     }
     axios({
       method:'post',
-      url: `${USER_API}/duplication/nickname`,
-      data:payload
-    })
-    .then((res)=>{
-      isNickDuplicated.value = res.data.isDuplicated
-      url:`${USER_API}/login/simple/nickname`,
-      data:payload
+      url:'${USER_API}/login/simple/nickname',
+      data:'payload'
     })
     .then((res)=>{
       accessToken.value = res.data.access-token
       refreshToken.value = res.data.refresh-token
-      userIcon.value = res.data.icon
     })
     .catch((err)=>{
       console.log(err)
     })
+
   }
+
 
   // 아이디 중복확인
   const checkMemberId = function (memberId){
@@ -57,10 +54,10 @@ export const useUserStore = defineStore('counter', () => {
     })
   }
 
+
   // 회원가입
   const signUp = function (payload) {
     const { memberId, password1, password2, nickName } = payload
-
     axios({
       method: 'post',
       url: `${USER_API}/signup/`,
@@ -78,49 +75,41 @@ export const useUserStore = defineStore('counter', () => {
       .catch(err => console.log(err))
   }
 
- // 아이디 유효성 검사
- const validateId = function (memberId) {
+
+
+  // 아이디 유효성 검사
+  const validateId = function (memberId) {
   if (memberId === null) {
     return true
   }
-  // 아이디가 공백이면 유효하지 않음
   if (memberId === '') {
     console.log('아이디 공백')
     return false
   }
-
-  // 아이디가 영어, 숫자만으로 이루어졌는지 확인
   const regExp = /^[a-zA-Z0-9]+$/
   if (!regExp.test(memberId)) {
     console.log('아이디 영숫')
     return false
   }
-
-  // 아이디 길이가 6~12자 사이인지 확인
   const length = memberId.length
   if (length < 6 || length > 12) {
     console.log('아이디 길이')
     return false
   }
-
-  // 아이디가 유효함
   console.log('아이디 유효성 검사 통과')
   return true
-}
+  }
 
-// 비밀번호 유효성 검사
-const validatePassword = function (password) {
+
+  // 비밀번호 유효성 검사
+  const validatePassword = function (password) {
   if (password === null) {
     return true
   }
-  
-  // 비밀번호가 공백이면 유효하지 않음
   if (password === '') {
     console.log('비번 공백')
     return false
   }
-
-  // 비밀번호가 영어, 숫자, 특수문자를 모두 포함하는지 확인
   const containsEnglish = /[a-zA-Z]/.test(password);
   const containsNumber = /\d/.test(password);
   const containsSpecialChar = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/.test(password);
@@ -129,50 +118,43 @@ const validatePassword = function (password) {
     console.log('비밀번호는 영어, 숫자, 특수문자를 모두 포함해야 합니다.');
     return false;
   }
-
-  // 비밀번호 길이가 8자 이상, 30자 이하인지 확인
   const length = password.length
   if (length < 8 || length > 30) {
     console.log('비번 길이')
     return false
   }
-
-  // 비밀번호가 유효함
   console.log('비밀번호 유효성 검사 통과')
   return true
-}
+  }
 
-// 닉네임 유효성 검사
-const validateNickName = function (nickName) {
-  if (nickName === null) {
+
+  // 닉네임 유효성 검사
+  const validateNickName = function (nickName) {
+    if (nickName === null) {
+      return true
+    }
+
+    if (nickName.value === '') {
+      console.log('닉넴 공백')
+      return false
+    }
+    const regExp = /^[가-힣a-zA-Z0-9_]+$/
+    if (!regExp.test(nickName)) {
+      console.log('닉넴 한영숫특')
+      return false
+    }
+    const length = nickName.length
+    if (length < 4 || length > 16) {
+      console.log('닉넴 길이')
+      return false
+    }
+    console.log('닉네임 유효성 검사 통과')
     return true
-  }
+    }
 
-  // 닉네임이 공백이면 유효하지 않음
-  if (nickName.value === '') {
-    console.log('닉넴 공백')
-    return false
-  }
 
-  // 닉네임이 한글, 영어, 숫자, 특수문자 ( _ )로만 이루어졌는지 확인
-  const regExp = /^[가-힣a-zA-Z0-9_]+$/
-  if (!regExp.test(nickName)) {
-    console.log('닉넴 한영숫특')
-    return false
-  }
 
-  // 닉네임 길이가 4자 이상, 16자 이하인지 확인
-  const length = nickName.length
-  if (length < 4 || length > 16) {
-    console.log('닉넴 길이')
-    return false
-  }
-
-  // 닉네임이 유효함
-  console.log('닉네임 유효성 검사 통과')
-  return true
 }
-
   return {checkNickName,accessToken, refreshToken, kakaoAccessToken, kakaoSignUp, generalLogIn, simpleLogIn, signUp, checkMemberId,
     isNickDuplicated, isIdDuplicated, validateId, validatePassword, validateNickName }
 },{persist:true})
