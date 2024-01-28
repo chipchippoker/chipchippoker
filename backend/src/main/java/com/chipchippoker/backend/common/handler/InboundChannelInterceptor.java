f4fb30851a48633b25a2b91953928f0a6cb8b393
hwan2da <dla612@naver.com>
2024-01-28 17:08:47 +0900

    feat: HEART BEAT 메시지 처리 및 연결 종료 멱등성 처리

--- backend/src/main/java/com/chipchippoker/backend/common/handler/InboundChannelInterceptor.java
+++ backend/src/main/java/com/chipchippoker/backend/common/handler/InboundChannelInterceptor.java
@@ -6,6 +6,7 @@ import java.util.Objects;
 
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.SimpMessageType;
 import org.springframework.messaging.simp.stomp.StompCommand;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
@@ -20,7 +21,7 @@ import lombok.extern.slf4j.Slf4j;
 @Component
 @RequiredArgsConstructor
 @Slf4j
-public class StompHandler implements ChannelInterceptor {
+public class InboundChannelInterceptor implements ChannelInterceptor {
 	private final JwtUtil jwtUtil;
 	private final MemberRepository memberRepository;
 	private static final Map<String, String> sessionMap = new HashMap<>();
@@ -34,16 +35,37 @@ public class StompHandler implements ChannelInterceptor {
 		log.info("preSend");
 		StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
 		String sessionId = accessor.getSessionId();
-		if (StompCommand.CONNECT.equals(accessor.getCommand())) {
+		SimpMessageType messageType = accessor.getMessageType();
+
+		// 웹소켓이 이미 연결된 상태이고 연결을 확인하는 메시지를 전송하는 경우
+		if (SimpMessageType.HEARTBEAT.equals(messageType)) {
+			log.info("HEART BEAT");
+		}
+		/*
+		웹 소켓 연결을 시도하는 경우
+		1. 액세스 토큰을 확인
+		2. 액세스 토큰에서 닉네임을 추출한다.
+		3. 각 닉네임 당 하나의 세션을 보유할 수 있다.
+		 */
+		else if (StompCommand.CONNECT.equals(accessor.getCommand())) {
 			log.info("CONNECT");
 			log.info(accessor.getFirstNativeHeader("access-token"));
 			String nickname = jwtUtil.getNickName(accessor.getFirstNativeHeader("access-token"),
 				accessor.getFirstNativeHeader("refresh-token"));
 			sessionMap.put(sessionId, nickname);
-		} else if (StompCommand.SUBSCRIBE.equals(accessor.getCommand()) && Objects.requireNonNull(
+			log.info(nickname.concat("이 연결된 세션 ID는 ").concat(sessionId).concat(" 입니다."));
+		}
+		/*
+		회원 개인의 메시지함을 구독하는 경우
+		1. 커맨드가 구독이고 구독 목표가 member를 포함하는 지 확인한다.
+		2. 액세스 토큰에서 닉네임을 추출한다.
+		3. 구독하려는 메시지함과 메시지를 보낸 사람이 일치하지 않는 경우 NULL 을 반환하여 구독을 할 수 없게한다.
+		 */
+		else if (StompCommand.SUBSCRIBE.equals(accessor.getCommand()) && Objects.requireNonNull(
 			accessor.getDestination()).contains("member")) {
 			log.info("SUBSCRIBE MEMBER");
-			String nickname = sessionMap.get(accessor.getSessionId());
+			String nickname = sessionMap.get(sessionId);
+			log.info("nickname : ".concat(nickname));
 			if (!nickname.equals(accessor.getDestination().split("/member/")[1])) {
 				return null;
 			}
@@ -69,7 +91,7 @@ public class StompHandler implements ChannelInterceptor {
 	connect 할 때 호출
 	subscribe 할 때 호출
 	disconnect 할 때 호출
-	클라이언트에게 메시지를 받을 때 호출
+	웹소켓 연결을 끊는 로직은 내부적으로 두 번 발생하기 때문에 멱등성을 위해 세션연결이 존재하는 지 추가했다
 	 */
 	@Override
 	public void postSend(Message<?> message, MessageChannel channel, boolean sent) {
@@ -77,8 +99,12 @@ public class StompHandler implements ChannelInterceptor {
 		StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
 		String sessionId = accessor.getSessionId();
 		log.info(sessionId);
-		if (StompCommand.DISCONNECT.equals(accessor.getCommand())) {
+		if (StompCommand.DISCONNECT.equals(accessor.getCommand())
+			&& !(sessionMap.get(sessionId) == null)) {
+			log.info("DISCONNECT");
+			String nickname = sessionMap.get(sessionId);
 			sessionMap.remove(sessionId);
+			log.info(nickname.concat("의 웹소켓의 연결을 종료합니다."));
 		}
 		ChannelInterceptor.super.postSend(message, channel, sent);
 	}
--- backend/src/main/java/com/chipchippoker/backend/config/WebSocketConfig.java
+++ backend/src/main/java/com/chipchippoker/backend/config/WebSocketConfig.java
@@ -1,13 +1,16 @@
 package com.chipchippoker.backend.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
-import com.chipchippoker.backend.common.handler.StompHandler;
+import com.chipchippoker.backend.common.handler.InboundChannelInterceptor;
 
 import lombok.RequiredArgsConstructor;
 
@@ -15,12 +18,16 @@ import lombok.RequiredArgsConstructor;
 @EnableWebSocketMessageBroker
 @RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-	private final StompHandler stompHandler;
+	private final InboundChannelInterceptor inboundChannelInterceptor;
 
 	@Override
 	public void configureMessageBroker(MessageBrokerRegistry registry) {
-		registry.setApplicationDestinationPrefixes("/to");
-		registry.enableSimpleBroker("/from");
+		registry
+			.setApplicationDestinationPrefixes("/to")
+			.enableSimpleBroker("/from")
+			.setHeartbeatValue(new long[] {10000L, 10000L});
+		// .setTaskScheduler(heartBeatScheduler());
+
 	}
 
 	@Override
@@ -32,7 +39,11 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
 	@Override
 	public void configureClientInboundChannel(ChannelRegistration registration) {
-		registration.interceptors(stompHandler);
+		registration.interceptors(inboundChannelInterceptor);
 	}
 
+	@Bean
+	public TaskScheduler heartBeatScheduler() {
+		return new ThreadPoolTaskScheduler();
+	}
 }
