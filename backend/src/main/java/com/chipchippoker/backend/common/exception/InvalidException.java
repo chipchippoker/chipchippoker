68dee881335a8822b7b6ab3417bca16537702c47
임세환 <dla612@naver.com>
2024-01-29 14:45:49 +0900

    [백엔드] 게임 전체종료 게임관리 수정 및 예외처리

--- backend/src/main/java/com/chipchippoker/backend/common/exception/InvalidException.java
+++ backend/src/main/java/com/chipchippoker/backend/common/exception/InvalidException.java
@@ -1,11 +1,15 @@
 package com.chipchippoker.backend.common.exception;
 
-
 import com.chipchippoker.backend.common.dto.ErrorBase;
+import com.chipchippoker.backend.common.dto.MessageBase;
 
 public class InvalidException extends ChipChipPokerBaseException {
 
-    public InvalidException(ErrorBase errorBase) {
-        super(errorBase);
-    }
+	public InvalidException(ErrorBase errorBase) {
+		super(errorBase);
+	}
+
+	public InvalidException(MessageBase messageBase) {
+		super(messageBase);
+	}
 }
--- backend/src/main/java/com/chipchippoker/backend/websocket/game/dto/GameEndMessageResponse.java
+++ backend/src/main/java/com/chipchippoker/backend/websocket/game/dto/GameEndMessageResponse.java
@@ -1,13 +1,39 @@
-// package com.chipchippoker.backend.websocket.game.dto;
-//
-// import lombok.AllArgsConstructor;
-// import lombok.Builder;
-// import lombok.Data;
-// import lombok.NoArgsConstructor;
-//
-// @Data
-// @Builder
-// @NoArgsConstructor
-// @AllArgsConstructor
-// public class GameEndMessageResponse {
-// }
+package com.chipchippoker.backend.websocket.game.dto;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import com.chipchippoker.backend.websocket.game.model.MemberEndGameInfo;
+import com.chipchippoker.backend.websocket.game.model.MemberManager;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class GameEndMessageResponse {
+	List<MemberEndGameInfo> memberEndGameInfos = new ArrayList<>();
+
+	public static GameEndMessageResponse create(Collection<MemberManager> memberManagers) {
+		List<MemberEndGameInfo> result = new ArrayList<>();
+
+		for (MemberManager manager : memberManagers) {
+			MemberEndGameInfo memberEndGameInfo = new MemberEndGameInfo(manager.getMemberInfo().getNickname(),
+				manager.getMemberGameInfo().getHaveCoin() == 25 ? "무" :
+					manager.getMemberGameInfo().getHaveCoin() > 25 ? "승" : "패",
+				((manager.getMemberGameInfo().getHaveCoin() / 25) * 10) +
+					(manager.getMemberGameInfo().getHaveCoin() - 25)
+			);
+			result.add(memberEndGameInfo);
+		}
+
+		return GameEndMessageResponse.builder()
+			.memberEndGameInfos(result)
+			.build();
+	}
+}
