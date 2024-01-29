3aabb54edae5446073eb4cf5bb3c0557651c3083
선수연 <suninisy@gmail.com>
2024-01-29 09:58:02 +0900

    [백엔드] 빠른 게임 시작 응답 데이터 수정

--- backend/src/main/java/com/chipchippoker/backend/api/matching/model/dto/StartFriendlyMatchingResponse.java
+++ backend/src/main/java/com/chipchippoker/backend/api/matching/model/dto/StartFriendlyMatchingResponse.java
@@ -15,12 +15,15 @@ public class StartFriendlyMatchingResponse {
 	private Long roomId;
 	private String title;
 	private Integer totalParticipantCnt;
+	private Boolean isFirstParticipant;
 
-	public static StartFriendlyMatchingResponse startFriendlyMatchingResponse(GameRoom gameRoom) {
+	public static StartFriendlyMatchingResponse startFriendlyMatchingResponse(GameRoom gameRoom,
+		Boolean isFirstParticipant) {
 		return StartFriendlyMatchingResponse.builder()
 			.roomId(gameRoom.getId())
 			.title(gameRoom.getTitle())
 			.totalParticipantCnt(gameRoom.getTotalParticipantCnt())
+			.isFirstParticipant(isFirstParticipant)
 			.build();
 	}
 }
