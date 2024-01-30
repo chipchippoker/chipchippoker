6fd3112084e3ae0f3498e91c92ad7385b1114ef9
권순준 <hgfdsa4320@naver.com>
2024-01-30 17:36:18 +0900

    [백엔드] 게임 매칭 메시지 로직 변경

--- backend/src/main/java/com/chipchippoker/backend/websocket/matching/dto/GameMatchingMessageRequest.java
+++ backend/src/main/java/com/chipchippoker/backend/websocket/matching/dto/GameMatchingMessageRequest.java
@@ -7,6 +7,6 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class CompleteMatchingMessageRequest {
+public class GameMatchingMessageRequest {
 	private Integer countOfPeople;
 }
