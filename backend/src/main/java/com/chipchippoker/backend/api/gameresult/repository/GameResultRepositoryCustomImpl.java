107590cc8de6438a8c7e7015e817307ba7291aa8
권순준 <hgfdsa4320@naver.com>
2024-02-13 10:14:12 +0900

    [백엔드] 최근 전적 조회 최신순으로 변경

--- backend/src/main/java/com/chipchippoker/backend/api/gameresult/repository/GameResultRepositoryCustomImpl.java
+++ backend/src/main/java/com/chipchippoker/backend/api/gameresult/repository/GameResultRepositoryCustomImpl.java
@@ -2,32 +2,36 @@ package com.chipchippoker.backend.api.gameresult.repository;
 
 import java.util.List;
 
-import com.chipchippoker.backend.common.collection.GameResult;
-import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 
+import com.chipchippoker.backend.common.collection.GameResult;
+
+import lombok.RequiredArgsConstructor;
+
 @RequiredArgsConstructor
 public class GameResultRepositoryCustomImpl implements GameResultRepositoryCustom {
-    private final MongoTemplate mongoTemplate;
+	private final MongoTemplate mongoTemplate;
 
-    // @Override
-    // public Integer getCoin() {
-    //     GameResult gameResult = mongoTemplate.findOne(
-    //             Query.query(Criteria.where("memberAId").is(123456789)), GameResult.class
-    //     );
-    //     return gameResult.getMemberACoin();
-    // }
+	// @Override
+	// public Integer getCoin() {
+	//     GameResult gameResult = mongoTemplate.findOne(
+	//             Query.query(Criteria.where("memberAId").is(123456789)), GameResult.class
+	//     );
+	//     return gameResult.getMemberACoin();
+	// }
 
-    public List<GameResult> findRecentPlayList(String nickname) {
-        List<GameResult> recentPlayList = mongoTemplate.find(
-            Query.query(Criteria.where("memberList").elemMatch(Criteria.where("$eq").is(nickname))).limit(10),
-            GameResult.class);
-        return recentPlayList;
-    }
+	public List<GameResult> findRecentPlayList(String nickname) {
+		List<GameResult> recentPlayList = mongoTemplate.find(
+			Query.query(Criteria.where("memberList").elemMatch(Criteria.where("$eq").is(nickname)))
+				.with(Sort.by(Sort.Order.desc("createdAt"))).limit(10),
+			GameResult.class);
+		return recentPlayList;
+	}
 
-    // public List<GameResult> findRecentPlayList(){
-    //     List<GameResult> recentPlayList = mongoTemplate.find(Query.query(Criteria.where("memberAId").is()))
-    // }
+	// public List<GameResult> findRecentPlayList(){
+	//     List<GameResult> recentPlayList = mongoTemplate.find(Query.query(Criteria.where("memberAId").is()))
+	// }
 }
